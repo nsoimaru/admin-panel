4711d2cb49aed1f23cf96ba3b1cd333d90cfdd5b
nsoimaru <nsoimaru@gmail.com>
2023-01-31 21:20:12 +0200

    Add toggle for sidebar

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 const body = document.querySelector('body');
 const modeToggle = document.querySelector('.mode-toggle');
+const sidebarToggle = document.querySelector('.sidebar-toggle');
+const sidebar = document.querySelector('nav');
 const toggleText = document.querySelector('.link-name');
 
 modeToggle.addEventListener('click', () => {
     body.classList.toggle('dark');
     // TODO Add Light Theme text at toggle button
-}); 
+});
+
+sidebarToggle.addEventListener('click', () => {
+    sidebar.classList.toggle('close');
+});
